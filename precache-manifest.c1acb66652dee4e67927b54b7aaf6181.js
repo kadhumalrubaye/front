e197522bdde0369e2db7bd307e1a8ed51fd0e727
kadhum <kadhum.it@gmail.com>
2020-08-12 03:14:41 +0300

    nest front taskman first comiit

---
+++ precache-manifest.c1acb66652dee4e67927b54b7aaf6181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32519a8468736fec780c4dcb6ee6bf06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95175684ad55f910c9a4",
+    "url": "/static/css/main.2673913b.chunk.css"
+  },
+  {
+    "revision": "69ee0c2477c9e5cc6e9d",
+    "url": "/static/js/2.25d8462d.chunk.js"
+  },
+  {
+    "revision": "95175684ad55f910c9a4",
+    "url": "/static/js/main.5c1cf35d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
